c1e7b6c5d4ed2749547d830dc0fb0689c031b322
moonstone0514 <comengiee852@naver.com>
2025-07-24 20:48:03 +0900

    add: 클릭시 해당 장르에 대한 영화들 리스트로 반환

--- movieRanking/src/main/java/model/MovieModel.java
+++ movieRanking/src/main/java/model/MovieModel.java
@@ -22,9 +22,24 @@ public class MovieModel {
 		
 		return em.createQuery("select m from Movie m", Movie.class).getResultList();
 		
-		
-		
-		
+	}
+	
+	/**장르(카테고리) 클릭시 해당 장르에 대한 영화들 리스트로 반환
+	 * @param String genreName
+	 * @return
+	 */
+	
+	public static List<Movie> getMovieListByGenre(String genreName) {
+	    EntityManager em = DBUtil.getEntityManager();
+	    try {
+	        // genres 컬럼에 genreName이 포함된 데이터를 조회
+	        return em.createQuery(
+	                "SELECT m FROM Movie m WHERE m.genres LIKE :genreName", Movie.class)
+	                 .setParameter("genreName", "%" + genreName + "%")
+	                 .getResultList();
+	    } finally {
+	        em.close(); // 자원 정리
+	    }
 	}
 	
 	
